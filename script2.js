c7b62f3fb59356a974d9086e93d94d3bc328aa52
Ak200523 <anelle.earhart@gmail.com>
2024-06-16 13:38:38 +0400

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,25 @@
+doucumet.getElementById('search-button').addEventListener('click', function(){
+  const query = document.getElementById('search-input').value;
+  fetch('/search?q=${query}')
+  .then(response => response.json())
+  .then(data => {
+    const results = document.getElementbyId('results');
+    results.innerHTML = '';
+    data.recipes.forEach(recipe => {
+      const li = document.createElement('li');
+      li.textContent = recipe.name;
+      li.dataset.id = recipe.id;
+      li.addEventListener('click', function(){
+        fetch(`/recipe/${this.dataset.id}`)
+        .then(response => response.json())
+        .then(data => {
+          alert (`Ingredients: ${data.recipe.ingredients}\nInstructions: ${data.recipe.instructions}`);
+
+        )};
+      )};
+    results.appendChild(li);
+  )};
+)};
+)};
+
 
